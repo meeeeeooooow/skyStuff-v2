066f82ca915fd3f57b55a7bd48b24609fff0d0f6
belawastaken <heischkel.bela@gmail.com>
2026-04-09 20:20:56 +0200

    Remove deprecated pvConfig file for player stats

--- src/config/pvConfig.js
+++
@@ -1,8 +0,0 @@
-export const pvConfig = [
-  {
-    name: "Skyblock Level",
-    category: "Core Stats",
-    tags: ["level", "lvl", "skylevel"],
-    getValue: (player) => Math.floor(player.leveling.experience / 100)
-  }
-];
